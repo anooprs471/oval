8fe0f124782281b72cac34c5ae665d7ea230545c
lijith <creativequb@gmail.com>
2015-05-23 19:39:32 +0530

    temp

--- admin-edit-plan.php
+++ admin-edit-plan.php
@@ -8,7 +8,6 @@ use Philo\Blade\Blade;
 $views = __DIR__ . '/views';
 $cache = __DIR__ . '/cache';
 
-
 $blade = new Blade($views, $cache);
 
 $user = new UserAccounts;
@@ -19,7 +18,7 @@ $capsule = $user->getCapsule();
 
 $flash_msg = '';
 
-if($flash->hasFlashMessage()){
+if ($flash->hasFlashMessage()) {
 	$flash_msg = $flash->show();
 }
 
@@ -38,59 +37,71 @@ $form_data = array(
 	'max-all-session' => '',
 	'max-daily-session' => '',
 	'simult-use' => '',
-	'idle-timeout' => ''
+	'idle-timeout' => '',
 );
 
 $msg = '';
 
-if($user->isAdmin()){
+if ($user->isAdmin()) {
 
-	if(isset($_GET['plan-name'])){
+	if (isset($_GET['plan-name'])) {
 
 		$plan_name = filter_var(trim($_GET['plan-name']));
 
 		$plan_radgroupcheck = $capsule::table('radgroupcheck')
-		->where('groupname', '=', $plan_name)
-		->get();
+			->where('groupname', '=', $plan_name)
+			->get();
 
 		$plan_radgroupreply = $capsule::table('radgroupreply')
-		->where('groupname', '=', $plan_name)
-		->get();
+			->where('groupname', '=', $plan_name)
+			->get();
 
 		//get attributes from the database
-		if($plan_radgroupcheck != null){
+		if ($plan_radgroupcheck != null) {
 
 			foreach ($plan_radgroupcheck as $data) {
-				if($data['attribute'] == 'Max-All-Session'){
+				if ($data['attribute'] == 'Max-All-Session') {
+					if ($data['value'] > 3600 && $data['value'] <= 86400) {
+						$data['value'] = $data['value'] / 3600;
+					} elseif ($data['value'] > 86400 && $data['value'] <= 604800) {
+						$data['value'] = $data['value'] / 86400;
+					} elseif ($data['value'] > 604800 && $data['value'] <= 2628000) {
+						$data['value'] = $data['value'] / 604800;
+					}
 					$form_data['max-all-session'] = $data['value'];
 				}
-				if($data['attribute'] == 'Max-Daily-Session'){
+				if ($data['attribute'] == 'Max-Daily-Session') {
+					if ($data['value'] > 1 && $data['value'] <= 3600) {
+						$data['value'] = $data['value'] / 60;
+					} elseif ($data['value'] > 3600 && $data['value'] <= 86400) {
+						$data['value'] = $data['value'] / 3600;
+					}
 					$form_data['max-daily-session'] = $data['value'];
 				}
-				if($data['attribute'] == 'Simultaneous-Use'){
+				if ($data['attribute'] == 'Simultaneous-Use') {
 					$form_data['simult-use'] = $data['value'];
 				}
-				if($data['attribute'] == 'CS-Total-Octets-Daily'){
+				if ($data['attribute'] == 'CS-Total-Octets-Daily') {
 					$form_data['max-data-usage'] = $data['value'];
 				}
 			}
 
 		}
 
-		if($plan_radgroupreply != null){
+		if ($plan_radgroupreply != null) {
 
 			foreach ($plan_radgroupreply as $data) {
 
-				if($data['attribute'] == 'Idle-Timeout'){
+				if ($data['attribute'] == 'Idle-Timeout') {
 					$form_data['idle-timeout'] = $data['value'];
 				}
-				if($data['attribute'] == 'Session-Timeout'){
+				if ($data['attribute'] == 'Session-Timeout') {
 					$form_data['session-timeout'] = $data['value'];
 				}
-				if($data['attribute'] == 'Chillispot-Bandwidth-Max-Up'){
+				if ($data['attribute'] == 'Chillispot-Bandwidth-Max-Up') {
 					$form_data['upload-speed'] = $data['value'];
 				}
-				if($data['attribute'] == 'Chillispot-Bandwidth-Max-Down'){
+				if ($data['attribute'] == 'Chillispot-Bandwidth-Max-Down') {
 					$form_data['download-speed'] = $data['value'];
 				}
 
@@ -98,11 +109,11 @@ if($user->isAdmin()){
 
 		}
 
-	}	
+	}
 
-	if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-		if(empty($err)){
+		if (empty($err)) {
 
 			$download_speed = filter_var(trim($_POST['download-speed']));
 			$upload_speed = filter_var(trim($_POST['upload-speed']));
@@ -115,74 +126,73 @@ if($user->isAdmin()){
 			//insert into radgroupreply
 			//
 
-			if($download_speed != ''){
+			if ($download_speed != '') {
 				$capsule::table('radgroupreply')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'Chillispot-Bandwidth-Max-Down')
-				->update(['value' => $download_speed]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'Chillispot-Bandwidth-Max-Down')
+					->update(['value' => $download_speed]);
 			}
 
-			if($upload_speed != ''){
+			if ($upload_speed != '') {
 				$capsule::table('radgroupreply')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'Chillispot-Bandwidth-Max-Up')
-				->update(['value' => $upload_speed]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'Chillispot-Bandwidth-Max-Up')
+					->update(['value' => $upload_speed]);
 			}
 
-			if($idle_timeout != ''){
+			if ($idle_timeout != '') {
 				$capsule::table('radgroupreply')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'Idle-Timeout')
-				->update(['value' => $idle_timeout]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'Idle-Timeout')
+					->update(['value' => $idle_timeout]);
 			}
 
-			if($max_all_session != ''){
+			if ($max_all_session != '') {
 				$capsule::table('radgroupcheck')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'Max-All-Session')
-				->update(['value' => $max_all_session]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'Max-All-Session')
+					->update(['value' => $max_all_session]);
 			}
 
-			if($max_daily_session != ''){
+			if ($max_daily_session != '') {
 				$capsule::table('radgroupcheck')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'Max-Daily-Session')
-				->update(['value' => $max_daily_session]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'Max-Daily-Session')
+					->update(['value' => $max_daily_session]);
 			}
 
-			if($simult_use != ''){
+			if ($simult_use != '') {
 				$capsule::table('radgroupcheck')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'Simultaneous-Use')
-				->update(['value' => $simult_use]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'Simultaneous-Use')
+					->update(['value' => $simult_use]);
 			}
 
-			if($max_data_usage != ''){
+			if ($max_data_usage != '') {
 				$capsule::table('radgroupcheck')
-				->where('groupname', '=', $plan_name)
-				->where('attribute', '=', 'CS-Total-Octets-Daily')
-				->update(['value' => $max_data_usage]);
+					->where('groupname', '=', $plan_name)
+					->where('attribute', '=', 'CS-Total-Octets-Daily')
+					->update(['value' => $max_data_usage]);
 			}
 
 			$flash->add('Successfully updated');
-			header('Location: '.Config::$site_url.'admin-edit-plan.php?plan-name='.urlencode($plan_name));
+			header('Location: ' . Config::$site_url . 'admin-edit-plan.php?plan-name=' . urlencode($plan_name));
 		}
 
 	}
 
-
 	$data = array(
 		'type' => 'admin',
-		'site_url'=> Config::$site_url,
+		'site_url' => Config::$site_url,
 		'page_title' => "Edit Plan",
 		'name' => 'Administrator',
 		'msg' => $msg,
 		'flash' => $flash_msg,
 		'form_data' => $form_data,
 		'errors' => $err,
-		'plan_name' => $plan_name
+		'plan_name' => $plan_name,
 	);
-	echo $blade->view()->make('admin.edit-plan',$data);
-}else{
-	header('Location: '.Config::$site_url.'logout.php');
+	echo $blade->view()->make('admin.edit-plan', $data);
+} else {
+	header('Location: ' . Config::$site_url . 'logout.php');
 }
